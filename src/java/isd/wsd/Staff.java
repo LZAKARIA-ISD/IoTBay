8613119a9751deaf3c4432aac81dbddb38ef93c8
lucash-12554200 <55736681+lucash@users.noreply.github.com>
2020-06-07 09:33:57 +1000

    Update fix for customer and staff classes

--- src/java/isd/wsd/Staff.java
+++ src/java/isd/wsd/Staff.java
@@ -9,64 +9,6 @@ package isd.wsd;
  *
  * @author chris & lucas
  */
-<<<<<<< HEAD
-public class Staff {
-    private String email;
-    private String name;
-    private String password;
-    private String dob;
-    private String phone;
-
-    public Staff(String email, String name, String password, String phone, String dob) {
-        this.email = email;
-        this.name = name;
-        this.password = password;
-        this.phone = phone;
-        this.dob = dob;
-        
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-}
-=======
 public class Staff extends User {
 
     private String position;
@@ -85,4 +27,3 @@ public class Staff extends User {
     }
 
 }
->>>>>>> 5bbc72751c30e956fe1fed2303de06a0cb7db626
