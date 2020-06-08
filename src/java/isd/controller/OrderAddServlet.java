14671695224bf4a363c5bb916852f846023e8c5a
wisawapakorn <61954046+wisawapakorn@users.noreply.github.com>
2020-06-09 03:32:28 +1000

    Update OrderAddServlet.java

--- src/java/isd/controller/OrderAddServlet.java
+++ src/java/isd/controller/OrderAddServlet.java
@@ -1,61 +1,58 @@
-<%-- 
-    Document   : product
-    Created on : 07/06/2020, 3:46:26 PM
-    Author     : Sam
---%>
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * Sorakrit
+ */
+package isd.controller;
 
-<%@page import="isd.wsd.Product"%>
- <%@page import="isd.controller.OrderAddServlet"%>
-<%@page import="java.text.DecimalFormat"%>
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Product Page</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <%
-           
-            String update = (String)session.getAttribute("productUpdate");
-            String priceErr = (String)session.getAttribute("priceErr");
-            String quantityErr = (String)session.getAttribute("quantityErr");
-            String productNameErr = (String)session.getAttribute("productNameErr");
-            String detailErr = (String)session.getAttribute("detailErr");
-            String typeErr = (String)session.getAttribute("typeErr");
-            Product product = (Product)session.getAttribute("product");
-            DecimalFormat priceFormatter = new DecimalFormat("$#0.00");
-        %>
-        <div class="container-fluid">
-            <div class="register">
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="text-center">${product.name}</h1>
-                        <h6>ID: ${product.id}</h6>
-                        <p>${product.type}</p>
-                        <p>${product.detail}</p>
-                        <p><%= priceFormatter.format(product.getPrice()) %></p>
-                        <!--Link to order page-->
-                        <form class="form-inline justify-content-center" action="orderHistory.jsp" method="post">
-                            <div class="form-group">
-                                <label class="mr-2" for="quantity">Quantity: </label>
-                                <input type="text" class="form-control mr-2" id="quantity" name="quantity">
-                            </div>
-                            <div class="mr-2">
-                                <input type="submit" value="Order" name="saved" class="btn btn-outline-primary">
-                                <a href="ProductCollectionServlet" class="btn btn-outline-danger">Cancel</a>
-                            </div>
-                            <input placeholder="<%= (productNameErr != null ? productNameErr : "") %>" value="<%= (productNameErr != null ? "" : product.getName()) %>" type="hidden" class="form-control" id="name" name="name" required="">
-                                <input placeholder="<%= (priceErr != null ? priceErr : "") %>" value="<%= (priceErr != null ? "" : product.getPrice()) %>" type="hidden" class="form-control" id="price" name="price" required="">
-                                <input placeholder="<%= (typeErr != null ? typeErr : "") %>" value="<%= (typeErr != null ? "" : product.getType()) %>" type="hidden" class="form-control" id="type" name="type" required="">                                
-                                <input placeholder="<%= (detailErr != null ? detailErr : "") %>" value="<%= (detailErr != null ? "" : product.getDetail()) %>" type="hidden" class="form-control" id="detail" name="detail" required="">
-                                <input value="${product.id}" type="hidden" class="form-control-plaintext" id="id" name="id" readonly>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+import isd.wsd.dao.DBOrderManager;
+import isd.wsd.dao.ProductDBManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import isd.wsd.Order;
+import isd.wsd.Product;
+
+/**
+ *
+ * @author Sam
+ */
+public class OrderAddServlet extends HttpServlet {
+
+    @Override   
+     protected void doPost(HttpServletRequest request, HttpServletResponse response)   throws ServletException, IOException {       
+             
+             HttpSession session = request.getSession();  
+             ProductDBManager productManager = (ProductDBManager)session.getAttribute("productManager");
+             DBOrderManager ordManager = (DBOrderManager)session.getAttribute("ordManager");
+             Validator validator = new Validator();
+             Order order = null;
+              int id = Integer.parseInt(request.getParameter("id"));
+             String name = request.getParameter("name");
+             String detail = request.getParameter("detail");
+             String type = request.getParameter("type"); 
+             double price = Double.parseDouble(request.getParameter("price"));
+             int quantity = Integer.parseInt(request.getParameter("quantity"));
+               validator.clear(session);
+            
+            
+                     try {       
+                         ordManager.addOrder(id, name, detail, quantity, price, type);
+                         session.setAttribute("order", order);
+                         response.sendRedirect("orderHistory.jsp");
+                        
+                     } catch (SQLException ex) {           
+                           Logger.getLogger(AddProductServlet.class.getName()).log(Level.SEVERE, null, ex);       
+                     }
+             
+            
+      }
+     }
