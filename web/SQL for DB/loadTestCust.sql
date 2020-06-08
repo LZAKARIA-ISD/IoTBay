bb26766bffdf9827a67673e06663fa3a668dc10d
lucash-12554200 <55736681+lucash@users.noreply.github.com>
2020-06-08 13:25:13 +1000

    added test sqls

---
+++ web/SQL for DB/loadTestCust.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Sam
+ * Created: 05/06/2020
+ */
+
+INSERT INTO CUSTOMERS (CUSEMAIL, CUSNAME, CUSPASSWORD, CUSPHONE, CUSTYPE)
+VALUES
+('test@test.com', 'John Smith', 'test', '0123456789', 'individual')
---
+++ web/SQL for DB/loadTestStaff.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Sam
+ * Created: 05/06/2020
+ */
+
+INSERT INTO STAFF (STAFFEMAIL, STAFFNAME, STAFFPASSWORD, STAFFPHONE, STAFFPOS)
+VALUES
+('test@iotbay.com', 'Mary Jane', 'test', '0123456789', 'product manager')
